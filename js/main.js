65b1efb3cbd9a428ee1365c4d130eb0f3e329d3b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-04 15:33:21 +0100

    Esercizio completo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Creare una password per l'utente
+
+// chiedere all'utente il proprio nome
+
+let firstName = prompt ("scrivi il tuo nome");
+
+console.log(firstName);
+
+// chiedere all'utente il proprio cognome
+let surname = prompt ("scrivi il tuo cognome");
+
+console.log(surname);
+
+
+// chiedere all'utente il suo colore preferito 
+let colorPrefe = prompt ("scrivi il tuo colore preferito");
+
+console.log(colorPrefe);
+
+// mostrargli la password creata
+document.getElementById("password").innerHTML = `la tua password è: ${firstName}${surname}${colorPrefe}21`;
